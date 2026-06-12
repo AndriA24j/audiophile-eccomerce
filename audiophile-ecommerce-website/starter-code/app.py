d833bbd47c224e4a62a239b1f09d398329ca9400
AndriA24j <andriadidize2010@gmail.com>
2026-06-12 20:02:33 +0400

    fix flask app and requirements

--- audiophile-ecommerce-website/starter-code/app.py
+++ audiophile-ecommerce-website/starter-code/app.py
@@ -336,5 +336,4 @@ def add_to_cart(product_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(port=5000, debug=True)
